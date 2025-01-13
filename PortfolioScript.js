cfa139acd5f4a732a5ea5dc5d292941f642e0ddf
Jonas090405 <jonas-gissler@gmx.de>
2025-01-13 12:10:33 +0100

    script hatte keine funktion

--- PortfolioScript.js
+++
@@ -1,8 +0,0 @@
-// Hole das Hamburger-Menü und das mobile Menü
-const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('nav ul.mobile');
-
-// Füge ein Event-Listener hinzu, der das Menü umschaltet, wenn auf das Hamburger-Icon geklickt wird
-hamburger.addEventListener('click', () => {
-  mobileMenu.classList.toggle('show');
-});
